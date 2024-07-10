b01f0c148069085f758f3455b04f5f8d5a98843c
MANTISKYU <asdaron44@gmail.com>
2024-07-10 14:35:24 +0900

    interceptor

---
+++ chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/MenuService.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+
+
+    public void method() {
+
+        System.out.println("MenuService 호출함");
+
+
+    }
+}
---
+++ chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/StopWatchInterceptor.java
@@ -0,0 +1,53 @@
+package com.ohgiraffers.interceptor;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.w3c.dom.ls.LSOutput;
+
+@Component
+public class StopWatchInterceptor implements HandlerInterceptor {
+
+    private final MenuService menuService;
+
+    public StopWatchInterceptor(MenuService menuService) {
+
+        this.menuService = menuService;
+
+    }
+
+
+    // 마지막에 호출하는 메소드
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion 호출함");
+        menuService.method();
+    }
+
+    // 후처리
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandler 호출함");
+
+        long startTime = (long) request.getAttribute("startTime");
+        request.removeAttribute("startTime");
+
+        long endTime = System.currentTimeMillis();
+
+        modelAndView.addObject("interval", endTime-startTime);
+    }
+
+    // 전처리
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandler 호출함");
+        long startTime = System.currentTimeMillis();
+
+        request.setAttribute("startTime", startTime);
+        // true면 컨트롤러를 이어서 호출한다. false이면 컨트롤러를 호출하지 않음.
+        return true;
+    }
+}
